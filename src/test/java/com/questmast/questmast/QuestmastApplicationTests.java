6c8a4e93b528561f73f74b6b0707d3aedb5bdbda
Andre Luis Correa <andreluiscorrea2003@gmail.com>
2024-12-13 07:45:54 -0300

    feat: create initial spring project for questmast api

---
+++ src/test/java/com/questmast/questmast/QuestmastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.questmast.questmast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestmastApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
