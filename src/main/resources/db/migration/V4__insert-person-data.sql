74916e8bfdf9a0718f76364a9309d2644a0f2976
Andre Luis Correa <andreluiscorrea2003@gmail.com>
2025-02-20 21:31:05 -0300

    feat: add authentication

--- src/main/resources/db/migration/V4__insert-person-data.sql
+++ src/main/resources/db/migration/V4__insert-person-data.sql
@@ -3,5 +3,5 @@ INSERT INTO gender (acronym, description) VALUES
                                         ('F', 'Feminino'),
                                         ('O', 'Outro');
 
-INSERT INTO app_user(password, person_role, username) VALUES
-                                                          ('$2a$10$G5mWSx6w6wYbC79sdMOFzectDP48aEK9XFJYbUu8iljkAo//ZoaAi', 'ROLE_ADMIN', 'admin');
+INSERT INTO app_user(password, person_role, username, is_email_verified) VALUES
+                                                          ('$2a$10$G5mWSx6w6wYbC79sdMOFzectDP48aEK9XFJYbUu8iljkAo//ZoaAi', 'ROLE_ADMIN', 'admin', true);
