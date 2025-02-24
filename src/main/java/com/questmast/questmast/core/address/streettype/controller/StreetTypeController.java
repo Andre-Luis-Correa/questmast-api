2068e314c3d7ea64151e3d5e374ff91eb87d9a96
André Correa <andre.cc@itaipuparquetec.org.br>
2025-02-24 17:15:43 -0300

    refactor: change address components folder

--- src/main/java/com/questmast/questmast/core/address/streettype/controller/StreetTypeController.java
+++ src/main/java/com/questmast/questmast/core/address/streettype/controller/StreetTypeController.java
@@ -5,6 +5,7 @@ import com.questmast.questmast.core.address.streettype.service.StreetTypeService
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class StreetTypeController {
 
     private final StreetTypeService streetTypeService;
 
+    @GetMapping
     public ResponseEntity<List<StreetType>> list() {
         return ResponseEntity.ok(streetTypeService.findAll());
     }
