744d63c455737d7a3f750b4bfa4e38a3073798ec
Andre Luis Correa <andreluiscorrea2003@gmail.com>
2025-02-24 11:58:02 -0300

    feat: add authentication

--- src/main/java/com/questmast/questmast/core/selectionprocesstest/mapper/SelectionProcessTestMapper.java
+++ src/main/java/com/questmast/questmast/core/selectionprocesstest/mapper/SelectionProcessTestMapper.java
@@ -1,10 +1,8 @@
 package com.questmast.questmast.core.selectionprocesstest.mapper;
 
-import com.questmast.questmast.core.selectionprocesstest.domain.model.SelectionProcessTest;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface SelectionProcessTestMapper {
 
-    SelectionProcessTest convert();
 }
